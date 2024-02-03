fd2e6436d9d0b006052e20442c9dcf8aae657002
starjardin <tantely.and@onja.org>
2024-02-03 11:12:59 +0300

    [challenge]: Convert string to number

---
+++ 5-convertStringToNumber/convertStringToNumber.js
@@ -0,0 +1,15 @@
+function numberToString(num) {
+	return num + "";
+}
+
+function numberToString2(num) {
+	return num.toString();
+}
+
+function numberToString3(num) {
+	return String(num);
+}
+
+function numberToString4(num) {
+	return `${num}`;
+}
