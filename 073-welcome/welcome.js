eac7f5bba2375675ff5e1b7e2086debd0749541b
starjardin <tantely.and@onja.org>
2024-06-04 09:57:47 +0300

    [challenge] Welcome

---
+++ 073-welcome/welcome.js
@@ -0,0 +1,30 @@
+
+const languages = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso",
+ }
+ 
+ function greet(language) {
+   if (!languages[language]) {
+     return 'Welcome'
+   }
+   
+   return `${languages[language]}`
+ }
+
+ 
